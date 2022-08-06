e430a1f90507e5e38b27e9b2c59ac3db4caa3983
YOGOH2020 <60884819+YOGOH2020@users.noreply.github.com>
2022-08-06 14:27:47 -0400

    Add files via upload

---
+++ doc.js
@@ -0,0 +1,84 @@
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}
+
+ // Function to check Whether both passwords
+// is same or not.
+function checkPassword(form) {
+  password = form.password.value;
+  confirmPassword = form.confirmPassword.value;
+  
+// If password not entered
+  if (password == '')
+  alert ("Please enter Password");
+                      
+// If confirm password not entered
+  else if (confirmPassword == '')
+    alert ("Please enter confirm password");
+                      
+// If Not same return False.    
+  else if (password != confirmPassword) {
+  alert ("\nPassword did not match: Please try again...")
+    return false;
+  }
+  
+// If same return True.
+ else{
+  alert("Password Match: Welcome to To do list!")
+  return true;
+  }
+    }
